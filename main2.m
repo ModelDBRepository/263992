4e6b1f61cca64ffbc105ece671466a0cbd9db1a6
Tom Morse <tom.morse@yale.edu>
2020-04-18 11:10:01 -0400

    added screenshots to the readme, modified main2.m to the 2 figs

--- main2.m
+++ main2.m
@@ -10,25 +10,29 @@ format short;clc;
 rng(938197);
 
 %parameters:
-G=0.1;mu0=5;
-par=parameters(G);bringparam(par);
-Iext=zeros(n,1);Tpulse=0.3;
-Iext(1:n1,1)=mu0;
-%run simulation:
-[v,calcium]=trial(par,Iext,Tpulse);
-
-
-
-figure('Position',[100,100,400,350]);
-time=dt:dt:triallength;time=time-transient; %we align with the stimulus onset
-F01=mean(calcium(1,transient/dt:(transient+0.9)/dt)); %baseline V1
-F02=mean(calcium(2,transient/dt:(transient+0.9)/dt)); %baseline AL
-plot(time,(calcium(1,:)-F01)/F01,'LineWidth',2,'Color',[.7 .3 .4]);hold on;
-plot(time,(calcium(2,:)-F02)/F02,'LineWidth',2,'Color',[.9 .7 .1]);
-xlabel('Time (s)');xlim([0 3]);ylim([0 .4])
-ylabel('Modelled response (\DeltaF/F)');set(gca,'box','off')
-legend([{'V1','AL'}]);
-
-
+G=0.1;
+mu0_vec=[5 12];
+for mu0_index=1:2
+    mu0=mu0_vec(mu0_index);
+    
+    par=parameters(G);bringparam(par);
+    Iext=zeros(n,1);Tpulse=0.3;
+    Iext(1:n1,1)=mu0;
+    %run simulation:
+    [v,calcium]=trial(par,Iext,Tpulse);
+    
+    
+    
+    figure('Position',[100,100,400,350]);
+    time=dt:dt:triallength;time=time-transient; %we align with the stimulus onset
+    F01=mean(calcium(1,transient/dt:(transient+0.9)/dt)); %baseline V1
+    F02=mean(calcium(2,transient/dt:(transient+0.9)/dt)); %baseline AL
+    plot(time,(calcium(1,:)-F01)/F01,'LineWidth',2,'Color',[.7 .3 .4]);hold on;
+    plot(time,(calcium(2,:)-F02)/F02,'LineWidth',2,'Color',[.9 .7 .1]);
+    xlabel(['(mu0=' num2str(mu0) '), Time (s)']);xlim([0 3]);ylim([0 .4])
+    ylabel('Modelled response (\DeltaF/F)');set(gca,'box','off')
+    legend([{'V1','AL'}]);
+    
+end
 
 
